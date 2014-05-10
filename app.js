a621fce0554a991f8a8b94acd26e2b4bb93b166d
Ziyu <odduid@gmail.com>
2014-05-10 13:48:39 +1000

    fix production error handler

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error', {
+  res.send({
     message: err.message,
     error: {}
   });
